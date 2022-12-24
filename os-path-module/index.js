9122ba4193e1a3899d1e0b95fc25ba52526db96c
mdhamidulislamT <hamidlive6@gmail.com>
2022-12-25 00:18:00 +0600

    Node os and paht module

---
+++ os-path-module/index.js
@@ -0,0 +1,24 @@
+// os , path
+
+//const os = require('os');
+//const {freemem, hostname} = require('os');
+
+//console.log(os.userInfo());
+//console.log(os.homedir());
+//console.log(os.hostname());
+//console.log(os.totalmem());
+//console.log(os.freemem());
+
+/* console.log(hostname());
+console.log(freemem()); */
+
+
+
+const path = require('path');
+
+const exenstionName = path.extname("index.html");
+const exenstionName2 = path.extname("index.js");
+console.log(exenstionName);
+console.log(exenstionName2);
+
+
