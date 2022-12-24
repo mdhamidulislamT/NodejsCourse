d139234c4dd5f1c95a5fd4a31596d533ae813ccc
mdhamidulislamT <hamidlive6@gmail.com>
2022-12-25 01:14:19 +0600

    Node external module

---
+++ Lesson5/index.js
@@ -0,0 +1,9 @@
+
+/* const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName()); */
+
+var moviesNames = require('movies-names');
+
+// console.log(moviesNames.random());
+console.log(moviesNames.random(3));
+
