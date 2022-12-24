56dbee68e9356424aa5d75abad332c3d3b0454ad
mdhamidulislamT <hamidlive6@gmail.com>
2022-12-24 23:36:20 +0600

    custome node module

--- Lesson/index.js
+++ Lesson/index.js
@@ -1,2 +1,9 @@
-console.log("Hello World");
-console.log("I am learnig Node js");
+/* const s1 = require('./student'); */
+ const {getName} = require('./student'); 
+
+/* console.log(s1.getName());
+console.log(s1.getAge());
+//console.log(s1.testYear);
+console.log(s1.year); */
+
+console.log(getName());
---
+++ Lesson/student.js
@@ -0,0 +1,24 @@
+/* const getName =()=>{
+    return "Anisul Islam";
+} */
+ exports.getName =()=>{
+    return "Anisul Islam";
+}
+
+const getAge =()=>{
+    return "25";
+}
+const year = 2022;
+
+//=== 1 ===//
+ /* exports.getName = getName;
+exports.getAge = getAge;
+exports.testYear = year;  */
+
+//=== 2 ===//
+
+/*  module.exports = {
+    getName,
+    getAge,
+    year
+} */
