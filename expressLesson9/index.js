8cf3bd85ee22c9d50a28d353dc1c215c260dd74b
mdhamidulislamT <hamidlive6@gmail.com>
2022-12-25 21:26:48 +0600

    form submit in post method in express

--- expressLesson9/index.js
+++ expressLesson9/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3003;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
-    const id = req.header('id');
-    const name = req.header('name');
-    res.send(`<h1>This is Home route, student id ${id} and name ${name}</h1>`)
+    res.send(`<h1>This is Home route</h1>`)
+})
+
+app.get('/register', (req, res) =>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post('/register', (req, res) =>{
+    const {fullName ,age} =req.body;
+    res.send(`<h1> ${fullName} and ${age} <br> register Form submitted successfully! </h1>`)
 })
 
 app.listen(PORT, () => {
