a5caef85a0f870f6e11c82cf010435cf212f7f78
mdhamidulislamT <hamidlive6@gmail.com>
2023-01-01 23:53:37 +0600

    mongo express Schema, model, create , update done

--- Mongodb-CRUD/index.js
+++ Mongodb-CRUD/index.js
@@ -5,9 +5,92 @@ const app = express();
 
 const PORT = 3002;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// create product schema
+const productsSchema = new mongoose.Schema({
+  title: {
+    type : String,
+    required: true
+  },
+  price: {
+    type : Number,
+    required: true
+  },
+  description: {
+    type : String,
+    required: true
+  },
+  rating: {
+    type : Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+// create product model
+const Product = mongoose.model('Products', productsSchema);
+
+
 app.get("/", (req, res) => {
   res.send("Welcome to mongodb home page");
 });
+
+// Create Product
+app.post("/products", async (req, res) => {
+  
+  try {
+    const { title, price, description } = req.body;
+    const newProduct = new Product({
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+      rating: req.body.rating
+    })
+
+    const productData = await newProduct.save();
+
+    res.send(productData);
+
+  } catch (error) {
+    res.status(500).send({
+      message: error.message
+    })
+  }
+
+});
+
+// Update Product
+app.put("/products/:id", async (req, res) => {
+  
+  try {
+  
+    const id = req.params.id;
+    const updateaProduct = await Product.findByIdAndUpdate({_id: id}, {
+      $set :{
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        rating: req.body.rating
+      },
+    },
+    {new: true}
+    );
+
+    res.send(updateaProduct);
+
+  } catch (error) {
+    res.status(500).send({
+      message: error.message
+    })
+  }
+
+});
+
+// DATABASE -> collections -> document
 
 const connectDB = async () => {
   try {
